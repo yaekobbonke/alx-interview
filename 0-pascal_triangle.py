4b476ccb6b269b61346dcbba93d62dcb36bcddb4
yaekobbonke <yaekobbundure@gmail.com>
2023-10-01 00:45:18 +0300

    pascal triangle modified

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,16 +1,16 @@
 #n = int(input("Enter the number of rows: "))
 def pascal_triangle(n):
-    if n <=0:
+    if n <= 0:
         return []
     triangle = []
-    for r in range(n):
+    for row in range(n):
         inner_row = []
-        for j in range(r + 1):
-            if j == 0 or j == r:
+        for j in range(row + 1):
+            if j == 0 or j == row:
                 inner_row.append(1)
             else:
-                upper_row_left = triangle[r - 1][j - 1]
-                upper_row_right = triangle[r - 1][j]
+                upper_row_left = triangle[row - 1][j - 1]
+                upper_row_right = triangle[row - 1][j]
                 m = upper_row_left + upper_row_right
                 inner_row.append(m)
         triangle.append(inner_row)
