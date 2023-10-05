99f3e5e8b3ce15990e0b50a044011a8ff447dd77
yaekobbonke <yaekobbundure@gmail.com>
2023-10-05 22:31:35 +0300

    fixed pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 ''' Lockboxes'''
 
+
 def canUnlockAll(boxes):
     '''if all the boxes can be opened'''
     n = len(boxes)
-    visited = [False] * n  # 
-    visited[0] = True  
-    queue = [0]  
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
 
     while queue:
-        current_box = queue.pop(0)  
+        current_box = queue.pop(0)
         for key in boxes[current_box]:
             if key < n and not visited[key]:
-                visited[key] = True  
-                queue.append(key)  
+                visited[key] = True
+                queue.append(key)
 
     return all(visited)
