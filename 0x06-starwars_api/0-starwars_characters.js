fed4f3a44be85944db6aa49313ebf7f90c997851
yaekobbonke <yaekobbundure.gmail.com>
2023-11-13 05:17:34 +0300

    swapi

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,34 @@
-const request = require('request');
+#!/usr/bin/node
+const axios = require('axios');
 
 function getMovieCharacters(movieId) {
-    const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-    
-    request(apiUrl, (error, response, body) => {
-        if (response.statusCode === 200) {
-            const movieData = JSON.parse(body);
-            const characters = movieData.characters;
-            
-            characters.forEach((characterUrl) => {
-                request(characterUrl, (error, response, body) => {
-                    if (response.statusCode === 200) {
-                        const characterData = JSON.parse(body);
-                        console.log(characterData.name);
-                    } else {
-                        console.log(`Failed to retrieve character data: ${response.statusCode}`);
-                    }
-                });
-            });
-        } else {
-            console.log(`Failed to retrieve movie data: ${response.statusCode}`);
-        }
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+  axios.get(apiUrl)
+    .then((response) => {
+      const movieData = response.data;
+      const characters = movieData.characters;
+
+      characters.forEach((characterUrl) => {
+        axios.get(characterUrl)
+          .then((response) => {
+            const characterData = response.data;
+            console.log(characterData.name);
+          })
+          .catch((error) => {
+            console.log(`Failed to retrieve character data: ${error.response.status}`);
+          });
+      });
+    })
+    .catch((error) => {
+      console.log(`Failed to retrieve movie data: ${error.response.status}`);
     });
 }
 
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.log("Please provide a movie ID as a command-line argument.");
+  console.log('Please provide a movie ID as a command-line argument.');
 } else {
-    getMovieCharacters(movieId);
+  getMovieCharacters(movieId);
 }
