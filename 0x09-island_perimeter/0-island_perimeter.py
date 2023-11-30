81f60e52403e135db39e9b757b48e1046c7a0820
yaekobbonke <yaekobbundure.gmail.com>
2023-11-30 19:04:10 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island
+    """
+    if not grid or not grid[0]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Add 4 to the perimeter for each land cell
+
+                # Check if there is land adjacent to the current cell
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2  # Subtract 2 if there is land to the north
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2  # Subtract 2 if there is land to the west
+
+    return perimeter
